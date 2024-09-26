aa52befa936abf6899f56346de2b11c8cda0b242
Jalkhmo <ponthieujade@gmail.com>
2024-09-26 10:54:44 +0200

    comment model

--- model/comment.model.js
+++ model/comment.model.js
@@ -1,5 +1,35 @@
 /**
  * Créer ici le model pour post
  * 
- * Un post doit avoir au minimum : un message, une date, un userId (auteur du post) et un postId
+ * Un post doit avoir au minimum : 
+ * un message, 
+ * une date, 
+ * un userId (auteur du post) 
+ * un postId
  */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.UserId,
+    ref: 'User',
+    required: true,
+  },
+  postId: {
+    type: Schema.Types.PostId,
+    ref: 'Post',
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Comment', commentSchema);

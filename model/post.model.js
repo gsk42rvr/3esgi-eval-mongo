c8b1acd4db764473309310c40775f660639fa456
gsk42rvr <kpatchavi@icloud.com>
2024-09-26 11:08:36 +0200

    Création du model

--- model/post.model.js
+++ model/post.model.js
@@ -3,3 +3,23 @@
  * 
  * Un post doit avoir au minimum : un message, une date, un userId (auteur du post)
  */
+
+const mongoose = require("mongoose")
+
+
+const postSchema = mongoose.Schema({
+    userId : {
+        type: mongoose.Types.ObjectId,
+        ref: "Users",
+        required: true,
+    },
+    message :{
+        type : String,
+        required: false,
+    }
+},{
+    timestamps: true
+    }
+)
+
+module.exports = mongoose.model('Post', postSchema)
